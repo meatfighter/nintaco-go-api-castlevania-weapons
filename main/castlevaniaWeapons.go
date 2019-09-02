91981f1401878d15bd16e6cce16efd6b5ea40f0b
Michael Birken <o__1@hotmail.com>
2019-09-01 20:51:56 -0400

    creating

--- main/castlevaniaWeapons.go
+++ main/castlevaniaWeapons.go
@@ -1,9 +1,22 @@
 package main
 
-import "github.com/meatfighter/nintaco-go-api/nintaco"
+import (
+	"fmt"
+
+	"github.com/meatfighter/nintaco-go-api/nintaco"
+)
+
+const (
+	addressLives     = 0x002A
+	addressShots     = 0x0064
+	addressWhip      = 0x0070
+	addressHearts    = 0x0071
+	addressSubweapon = 0x015B
+)
 
 type castlevaniaWeapons struct {
-	api nintaco.API
+	api           nintaco.API
+	buttonPressed bool
 }
 
 func newCastlevaniaWeapons() *castlevaniaWeapons {
@@ -11,7 +24,60 @@ func newCastlevaniaWeapons() *castlevaniaWeapons {
 }
 
 func (c *castlevaniaWeapons) launch() {
+	c.api.AddFrameListener(c)
+	c.api.AddStatusListener(c)
+	c.api.AddActivateListener(c)
+	c.api.AddDeactivateListener(c)
+	c.api.AddStopListener(c)
+	c.api.Run()
+}
 
+func (c *castlevaniaWeapons) APIEnabled() {
+	fmt.Println("API enabled")
+}
+
+func (c *castlevaniaWeapons) APIDisabled() {
+	fmt.Println("API disabled")
+}
+
+func (c *castlevaniaWeapons) Dispose() {
+	fmt.Println("API stopped")
+}
+
+func (c *castlevaniaWeapons) StatusChanged(message string) {
+	fmt.Printf("Status message: %s\n", message)
+}
+
+func (c *castlevaniaWeapons) switchWeapons() {
+	c.api.WriteCPU(addressSubweapon, c.getNextSubweapon())
+}
+
+func (c *castlevaniaWeapons) getNextSubweapon() int {
+	switch c.api.ReadCPU(addressSubweapon) {
+	case 0x08:
+		return 0x09 // dagger -> cross
+	case 0x09:
+		return 0x0B // cross -> holy water
+	case 0x0B:
+		return 0x0D // holy water -> axe
+	case 0x0D:
+		return 0x0F // axe -> stopwatch
+	case 0x0F:
+		return 0x0A // stopwatch -> rosary
+	default:
+		return 0x08 // rosary -> dagger
+	}
+}
+
+func (c *castlevaniaWeapons) FrameRendered() {
+	if c.api.ReadGamepad(0, nintaco.GamepadButtonSelect) {
+		if !c.buttonPressed {
+			c.buttonPressed = true
+			c.switchWeapons()
+		}
+	} else {
+		c.buttonPressed = false
+	}
 }
 
 func main() {
